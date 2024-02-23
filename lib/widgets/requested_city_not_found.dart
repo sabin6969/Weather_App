5b65a1b890916bee314bcc6c34b28c5e522ab379
Sabin Poudel <sabinpoudel6969@gmail.com>
2024-02-23 20:26:04 +0530

    Refactor requested city not found widget

--- lib/widgets/requested_city_not_found.dart
+++ lib/widgets/requested_city_not_found.dart
@@ -17,41 +17,47 @@ class RequesteCityNotFound extends StatelessWidget {
     return SizedBox(
       height: size.height,
       width: size.width,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          TextFormField(
-            textCapitalization: TextCapitalization.sentences,
-            keyboardType: TextInputType.name,
-            onFieldSubmitted: (value) {
-              context.read<HomeBloc>().add(
-                    HomeLoadWeatherDataWithCity(
-                      city: cityNameController.text,
-                    ),
-                  );
-            },
-            controller: cityNameController,
-            decoration: InputDecoration(
-              prefixIcon: IconButton(
-                onPressed: () {
-                  context.read<HomeBloc>().add(
-                        HomeLoadWeatherDataWithCity(
-                          city: cityNameController.text,
-                        ),
-                      );
-                },
-                icon: Icon(
-                  Icons.search,
-                  color: Theme.of(context).iconTheme.color,
-                  size: Theme.of(context).iconTheme.size,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: size.width * 0.1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TextFormField(
+              textCapitalization: TextCapitalization.sentences,
+              keyboardType: TextInputType.name,
+              onFieldSubmitted: (value) {
+                context.read<HomeBloc>().add(
+                      HomeLoadWeatherDataWithCity(
+                        city: cityNameController.text,
+                      ),
+                    );
+              },
+              controller: cityNameController,
+              decoration: InputDecoration(
+                prefixIcon: IconButton(
+                  onPressed: () {
+                    context.read<HomeBloc>().add(
+                          HomeLoadWeatherDataWithCity(
+                            city: cityNameController.text,
+                          ),
+                        );
+                  },
+                  icon: Icon(
+                    Icons.search,
+                    color: Theme.of(context).iconTheme.color,
+                    size: Theme.of(context).iconTheme.size,
+                  ),
                 ),
+                hintText: "Search your city",
               ),
-              hintText: "Search your city",
             ),
-          ),
-          Lottie.asset(AppAnimationPath.weatherDataOfRequestedCityNotFound),
-          Text(state.errorMessage),
-        ],
+            Lottie.asset(AppAnimationPath.weatherDataOfRequestedCityNotFound),
+            Text(
+              state.errorMessage,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
